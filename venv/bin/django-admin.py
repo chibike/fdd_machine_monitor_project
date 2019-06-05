ee0320d66fa785560acd0af74408eb67fb243a70
Chibuike Okpaluba <cpraise@cryptoquantique.com>
2019-06-05 16:55:56 +0100

    created project

---
+++ venv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/mnt/c/Users/cokpaluba/Desktop/demonstartion_ws/demonstration_ws/venv/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
